afda11112a14ece53facad7f8a34d264cc6244a8
Daniel Barrientos Iglesias <uo270162@uniovi.es>
2022-01-18 19:24:17 +0100

    Arreglo 7 triángulos

--- js/sketch.js
+++ js/sketch.js
@@ -234,7 +234,7 @@ function mutateFigure(shape) {
       shape.height = shape.height - 0.1 * widthIncrement;
     }
   } else if (shape instanceof Triangle) {
-    shape.width = shape.width - 5/duration;
+    shape.width = shape.width - 500000/duration;
 
     if (shape.height < 0.75 * height)
       shape.height = shape.height + 0.01 * widthIncrement;
